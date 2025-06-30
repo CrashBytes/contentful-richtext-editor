d1d58a2c745d7501a0743878cb2d944ea5014833
Michael Eakins <crashbytes@blackholesoftware.com>
2025-06-29 23:26:17 -0400

    Upgraded vite and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,13 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
   typescript: {
     check: false,
     reactDocgen: 'react-docgen-typescript',
@@ -22,12 +17,13 @@ const config: StorybookConfig = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
+
   viteFinal: async (config) => {
     if (config.css) {
       config.css.postcss = null;
     }
     return config;
-  },
+  }
 };
 
 export default config;
--- src/components/ContentfulEditor.stories.tsx
+++ src/components/ContentfulEditor.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { action } from 'storybook/actions';
 import { ContentfulRichTextEditor } from './ContentfulEditor';
 import { testDataSamples } from '../testData/samples';
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
